8b1908e55b19a438a7fcdcce87ef53c69d940a8c
mindstorm38 <theorozier38@gmail.com>
2018-05-14 23:04:30 +0200

    Changing autoloader

--- src/autoloader.inc.php
+++ src/autoloader.inc.php
@@ -3,22 +3,25 @@
 spl_autoload_register( function( $path ) {
 
 	static $paths = [
-		"PHPHelper\\src\\Utils"							=> "./src/utils.class.php",
-		"PHPHelper\\src\\Config"						=> "./src/config.class.php",
-		"PHPHelper\\src\\Core"							=> "./src/core.class.php",
-		"PHPHelper\\src\\Lang"							=> "./src/lang.class.php",
-		"PHPHelper\\src\\SessionManager"				=> "./src/sessionmanager.class.php",
-		"PHPHelper\\src\\SessionHandler"				=> "./src/sessionmanager.class.php",
-		"PHPHelper\\src\\Database"						=> "./src/database.class.php",
-		"PHPHelper\\src\\SQLManager"					=> "./src/sqlmanager.class.php",
-		"PHPHelper\\src\\SQLSerializable"				=> "./src/sqlserializable.class.php",
-		"PHPHelper\\src\\Query"							=> "./src/querymanager.class.php",
-		"PHPHelper\\src\\QueryResponse"					=> "./src/querymanager.class.php",
-		"PHPHelper\\src\\QueryManager"					=> "./src/querymanager.class.php"
+		"PHPHelper\\src\\Utils"							=> "utils.class.php",
+		"PHPHelper\\src\\Config"						=> "config.class.php",
+		"PHPHelper\\src\\Core"							=> "core.class.php",
+		"PHPHelper\\src\\Lang"							=> "lang.class.php",
+		"PHPHelper\\src\\SessionManager"				=> "sessionmanager.class.php",
+		"PHPHelper\\src\\SessionHandler"				=> "sessionmanager.class.php",
+		"PHPHelper\\src\\Database"						=> "database.class.php",
+		"PHPHelper\\src\\SQLManager"					=> "sqlmanager.class.php",
+		"PHPHelper\\src\\SQLSerializable"				=> "sqlserializable.class.php",
+		"PHPHelper\\src\\Query"							=> "querymanager.class.php",
+		"PHPHelper\\src\\QueryResponse"					=> "querymanager.class.php",
+		"PHPHelper\\src\\QueryManager"					=> "querymanager.class.php"
 	];
 
-	$path = array_key_exists( $path, $paths ) ? $paths[ $path ] : null;
-	if ( $path != null && file_exists( $path ) && is_readable( $path ) ) {
+	if ( !array_key_exists( $path, $paths ) ) return;
+
+	$path = dirname( __FILE__ ) . DIRECTORY_SEPARATOR . $paths[ $path ];
+
+	if ( file_exists( $path ) && is_readable( $path ) ) {
 		require_once $path;
 	}
 
