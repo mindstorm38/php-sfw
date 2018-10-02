e8d93bbc0fa556649758c3f9a2fd92e4ac9110f8
mindstorm38 <theorozier38@gmail.com>
2018-10-02 21:24:28 +0200

    Added constructor and removed "_"

--- src/UIDBaseClass.php
+++ src/UIDBaseClass.php
@@ -6,12 +6,16 @@ abstract class UIDBaseClass {
 
 	protected $uid;
 
+	public function __construct() {
+		$this->uid = 0;
+	}
+
 	public function get_uid() {
 		return $this->uid;
 	}
 
 	public function set_uid( $uid ) {
-		$this->_uid = intval( $uid );
+		$this->uid = intval( $uid );
 	}
 
 }
