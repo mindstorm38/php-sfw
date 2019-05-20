10f451dc9e448d677a54584bd28c71ab677f829b
mindstorm38 <theorozier38@gmail.com>
2019-05-20 21:21:54 +0200

    Solved query route problem.

--- src/lib/Route/QueryRoute.php
+++ src/lib/Route/QueryRoute.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace SFW\Route;
 
@@ -14,24 +14,24 @@ class QueryRoute extends Route {
 		parent::__construct($action);
 		
 		$this->base_path = Utils::beautify_url_path($base_path);
-		$this->$base_path_len_min = strlen($this->base_path) + 1;
+		$this->base_path_len_min = strlen($this->base_path) + 1;
 		
 	}
 	
 	public function identifier() : string {
 		return "{$this->base_path}<QUERY>";
 	}
-
+	
 	protected function routable( string $path, string $bpath ) : ?array {
 		
-		if ( Utils::starts_with( $bpath, "{$this->base_path}/" ) && strlen($bpath) > $this->$base_path_len_min && strpos( $bpath, "/", $this->$base_path_len_min ) === false ) {
-			return [ substr( $bpath, $this->$base_path_len_min ) ];
+		if ( Utils::starts_with( $bpath, "{$this->base_path}/" ) && strlen($bpath) > $this->base_path_len_min && strpos( $bpath, "/", $this->base_path_len_min ) === false ) {
+			return [ substr( $bpath, $this->base_path_len_min ) ];
 		}
 		
 		return null;
 		
 	}
-
+	
 }
 
 ?>
