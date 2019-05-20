48c0203f10bf44c118b3930a3cd6bd23a23958b7
mindstorm38 <theorozier38@gmail.com>
2019-05-20 13:10:10 +0200

    Worked on error page.

--- src/pages/error/content.php
+++ src/pages/error/content.php
@@ -1,1 +1,9 @@
-error content
+<?php
+
+use SFW\Lang;
+
+?>
+
+<h1><?= Lang::get("error.code_message", [ $page->vars["code"] ]) ?></h1>
+<h3><?= Lang::get("http_status.{$page->vars["code"]}")  ?></h3>
+<h4><?= $page->vars["msg"] ?></h4>
