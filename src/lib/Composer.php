f63acda33e9cb3ec16624c70818d7bdd505008f2
mindstorm38 <theorozier38@gmail.com>
2020-05-07 12:04:16 +0200

    Solved wrong regexp for host validation.

--- src/lib/Composer.php
+++ src/lib/Composer.php
@@ -90,8 +90,8 @@ final class Composer {
 	}
 
 	private static function valid_host(string $host): bool {
-	    return preg_match("^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$", $host) === 1 ||
-            preg_match("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", $host) === 1;
+	    return preg_match("/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/", $host) === 1 ||
+            preg_match("/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/", $host) === 1;
     }
 	
 	private static function get_default_ws_path() : string {
